f84df02649e802b40aaefdbd1d0a7725bb4779e6
Roy TS <royterjestrommen@hotmail.com>
2023-12-04 12:39:31 +0100

    update getting somewhere

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function randomAnimal() {
 
 console.log(randomAnimal());
 
+let currentImageIndex = 1;
+
+function showNextImage() {
+    const dyrBilde = document.getElementById('dyrBilde');
+    currentImageIndex++;
+
+    if (currentImageIndex > 10) {
+        currentImageIndex = 1;
+    }
+
+    const nextImageSrc = `image${currentImageIndex}.jpg`;
+    dyrBilde.src = nextImageSrc;
+}
